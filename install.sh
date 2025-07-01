80532f67fa497d62471b83fe0c9609dfb817c3ab
f1faq <f1faq@free.fr>
2025-07-01 11:54:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -142,6 +142,8 @@ sudo cp /home/pi/vidsource/tcdata10.txt /boot/testcard/tcdata10.txt
 sudo cp /home/pi/vidsource/tcdata11.txt /boot/testcard/tcdata11.txt
 sudo cp /home/pi/vidsource/tcdata12.txt /boot/testcard/tcdata12.txt
 sudo cp /home/pi/vidsource/tcdata13.txt /boot/testcard/tcdata13.txt
+sudo cp /home/pi/vidsource/tcdata14.txt /boot/testcard/tcdata14.txt
+sudo cp /home/pi/vidsource/tcdata15.txt /boot/testcard/tcdata15.txt
 
 sudo cp /home/pi/vidsource/11g720.jpg /boot/testcard/11g720.jpg
 sudo cp /home/pi/vidsource/75cb720.jpg /boot/testcard/75cb720.jpg
@@ -155,7 +157,8 @@ sudo cp /home/pi/vidsource/mire2.jpg /boot/testcard/mire2.jpg
 sudo cp /home/pi/vidsource/mire3.jpg /boot/testcard/mire3.jpg
 sudo cp /home/pi/vidsource/mire4.jpg /boot/testcard/mire4.jpg
 sudo cp /home/pi/vidsource/testcard640.jpg /boot/testcard/testcard640.jpg
-
+sudo cp /home/pi/vidsource/mireCQdeF1FAQblanc.jpg /boot/testcard/mireCQdeF1FAQblanc.jpg
+sudo cp /home/pi/vidsource/mireF1FAQblanc.jpg /boot/testcard/mireF1FAQblanc.jpg
 
 sudo cp /home/pi/vidsource/camtc23a.sh /boot/testcard/camtc23a.sh
 
